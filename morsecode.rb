84b7ced281e0ba65fb6128e0baac46897419567b
leloufadel <leloufadel@gmail.com>
2023-08-30 22:10:26 +0300

    fix linters

--- morsecode.rb
+++ morsecode.rb
@@ -1,27 +1,26 @@
-
 MORSE_CODE = {
-    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-    '--..' => 'Z', ' ' => ' '
-  }
-  
-  def decode_char(morse_char)
-    MORSE_CODE[morse_char]
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+  '--..' => 'Z', ' ' => ' '
+}
+
+def decode_char(morse_char)
+  MORSE_CODE[morse_char]
+end
+
+def decode_word(morse_word)
+  morse_chars = morse_word.split(" ")
+  decoded_word = ""
+  morse_chars.each do |morse_char|
+    decoded_char = decode_char(morse_char)
+    decoded_word += decoded_char
   end
-  
-  def decode_word(morse_word)
-    morse_chars = morse_word.split(" ")
-    decoded_word = ""
-    morse_chars.each do |morse_char|
-      decoded_char = decode_char(morse_char)
-      decoded_word += decoded_char
-    end
-    decoded_word
-  end
-  
+  decoded_word
+end
+
 #   def decode_message(morse_message)
 #     morse_words = morse_message.split("   ") # Three spaces indicate word separation
 #     decoded_message = ""
@@ -31,9 +30,8 @@ MORSE_CODE = {
 #     end
 #     decoded_message.strip # Remove leading/trailing spaces
 #   end
-  
+
 #   # Test the method with the old bottle message
 #   morse_message = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
 #   decoded_message = decode_message(morse_message)
 #   puts decoded_message
-  
