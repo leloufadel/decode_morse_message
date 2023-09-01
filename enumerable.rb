c1d3602582aa1291f9e28ac1e9ce8619878e1068
Asum Victor <iamasum369@gmail.com>
2023-09-01 08:01:49 +0000

    Refactor

--- enumerable.rb
+++ enumerable.rb
@@ -1,13 +1,13 @@
 module Enumerable
-  def all?(&block)
-    each { |item| return false unless block.call(item) }
-    true
-  end
+#   def all?(&block)
+#     each { |item| return false unless block.call(item) }
+#     true
+#   end
 
-  def any?(&block)
-    each { |item| return true if block.call(item) }
-    false
-  end
+#   def any?(&block)
+#     each { |item| return true if block.call(item) }
+#     false
+#   end
 
   def filter(&block)
     result = []
