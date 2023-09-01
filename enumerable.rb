cb509fa125c09c05d700b3dddf31940b1f7ef055
leloufadel <leloufadel@gmail.com>
2023-09-01 11:03:00 +0300

    Refactor

--- enumerable.rb
+++ enumerable.rb
@@ -1,8 +1,6 @@
 module Enumerable
-#   def all?(&block)
-#     each { |item| return false unless block.call(item) }
-#     true
-#   end
+ 
+  
 
 #   def any?(&block)
 #     each { |item| return true if block.call(item) }
